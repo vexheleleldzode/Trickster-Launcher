65d9f31e76b512855a6a22ea07e3742980a9ffd4
vexheleleldzode <heiliaopa@gmail.com>
2026-09-24 22:40:16 +0800

    build: update OpenSSL headers to version 3.5.0
    Sync internal OpenSSL headers and configuration to version 3.5.0 and update file generation metadata.

---
+++ Libs/openssl/include/openssl/fipskey.h
@@ -0,0 +1,41 @@
+/*
+ * WARNING: do not edit!
+ * Generated from /tmp/openssl-3.5.0/include/openssl/fipskey.h.in
+ *
+ * Copyright 2020-2024 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#ifndef OPENSSL_FIPSKEY_H
+# define OPENSSL_FIPSKEY_H
+# pragma once
+
+# ifdef  __cplusplus
+extern "C" {
+# endif
+
+/*
+ * The FIPS validation HMAC key, usable as an array initializer.
+ */
+#define FIPS_KEY_ELEMENTS \
+    
+
+/*
+ * The FIPS validation key, as a string.
+ */
+#define FIPS_KEY_STRING ""
+
+/*
+ * The FIPS provider vendor name, as a string.
+ */
+#define FIPS_VENDOR ""
+
+# ifdef  __cplusplus
+}
+# endif
+
+#endif
